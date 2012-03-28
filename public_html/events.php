0bbf608f7f9a09d84f81452d29784b6e0ba7b34c
Saki Sato <sakiisato@gmail.com>
2012-03-28 15:47:44 -0400

    added gallery and events pages, added styles, clearfixed homepage for firefox

--- public_html/events.php
+++ public_html/events.php
@@ -5,23 +5,13 @@
 <head>
 <!-- VARIBLES -->
     <?
-        $INDEXPAGE='YES';
-        $ACTIVEPAGE='home';
-        $PAGETITLE='Home';
+        $ACTIVEPAGE='events';
+        $PAGETITLE='Event Calendar';
     ?>
+    
 <!-- HEAD -->
     <? include("templates/head.php");?>
     
-    <script type="text/javascript" src="js/jquery.cycle.all.js"></script>
-    <script type="text/javascript">
-    $(document).ready(function() {
-        $('#slideshow').cycle({
-            fx: 'fade',
-            pager: '#slide_nav',
-            slideExpr: '.slide'
-        });
-    });
-    </script>
 </head>
 
 <body id="<? echo $ACTIVEPAGE; ?>">
@@ -30,25 +20,13 @@
 
 <!-- CONTENT -->
     <div id="content">
-        <section id="slideshow">
-            <div id="slide_nav">
-                <!-- Links added by cycle plugin -->
-            </div>
-            <div class="slide first">
-                <h2 class="slide_title">The Stage</h2>
-                <img src="images/superbowl-928x428.jpg" alt="The Stage">
-            </div>
-            <div class="slide">
-                <h2 class="slide_title">The Hotel</h2>
-                <img src="images/hotel-928x428.jpg" alt="The Hotel">
-            </div>
-            <div class="slide">
-                <h2 class="slide_title">The Bar</h2>
-                <img src="images/bar-928x428.jpg" alt="The Bar">
+        <section id="calendar_wrapper">
+            <h2 class="page_title"><? echo $PAGETITLE; ?></h2>
+            <div id="calendar">
             </div>
         </section>
         <section id="event_listing">
-            <h3>Upcoming Events</h3>
+            <h3 class="info_title">Upcoming Events</h3>
             <article class="event">
                 <a href="#">
                     <img src="http://www.placehold.it/120x80" width="120" height="80" alt="thumbnail" />
@@ -82,16 +60,6 @@
                     </div>
                 </a>
             </article>
-        </section>
-        <section id="restaurant">
-            <div class="wrapper">
-                <h2>Toshi's Restaurant</h2>
-                <p>Short Description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ut odio vitae est tempor congue. Nulla orci libero, molestie ut pulvinar sit amet, adipiscing sit amet purus. Praesent at nulla nec tortor sagittis vestibulum.</p>
-                <div class="button_wrapper">
-                    <a class="button red" href="#">View Menu</a>
-                    <a class="button red" href="#">Reservations</a>
-                </div>
-            </div>
         </section>
     </div>
 <!-- FOOTER -->
