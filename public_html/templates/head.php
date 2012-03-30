7a3d2b8e9a0ccd4caeb548caf4471bd98acdba8a
Saki Sato <sakiisato@gmail.com>
2012-03-29 22:01:29 -0400

    deleted unecessary scripts

--- public_html/templates/head.php
+++ public_html/templates/head.php
@@ -7,8 +7,6 @@
 <link rel="stylesheet" href="css/pages.css" type="text/css" media="all">
 
 <script type="text/javascript" src="js/jquery-1.7.1.min.js"></script>
-<script type="text/javascript" src="js/cufon-yui.js"></script>
-<script type="text/javascript" src="js/cufon-replace.js"></script> 
 
 <!--[if lt IE 9]>
     <script type="text/javascript" src="js/html5.js"></script>
