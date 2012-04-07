f33e0a44f156eabe8bc97c39cb82b4e63e3d756b
Saki Sato <sakiisato@gmail.com>
2012-04-06 21:28:59 -0400

    changed order, commented out newsletter link, changed account to my account

--- public_html/templates/header.php
+++ public_html/templates/header.php
@@ -7,8 +7,9 @@
     <nav id="top_nav">
         <ul>
             <? if(!$_SESSION['UsErId'] || $_SESSION['UsErId']<0){?><!-- No Welcome -->
-            <? }else{?><li>Welcome <a href="myaccount.php" style="color:#CCCCCC;"><?=ucfirst(stripslashes(GetName1("users","firstname","id",$_SESSION['UsErId'])));?> <?=ucfirst(stripslashes(GetName1("users","lastname","id",$_SESSION['UsErId'])));?></a></li><? } ?>
-            <li><a href="#">Get Our Newsletter</a></li>
+            <? }else{?><li>Welcome <a href="my_account.php" >       <?=ucfirst(stripslashes(GetName1("users","firstname","id",$_SESSION['UsErId'])));?> <?=ucfirst(stripslashes(GetName1("users","lastname","id",$_SESSION['UsErId'])));?>
+                </a></li><? } ?>
+            <!-- <li><a href="#">Get Our Newsletter</a></li> -->
             <li <? if($ACTIVEPAGE=='cart'){?>class="active"<? }?> ><a href="viewcart.php">Cart</a></li>
             <li <? if(!$_SESSION['UsErId'] || $_SESSION['UsErId']<0){?>
                 <? if($ACTIVEPAGE=='login'){?>class="active"<? }?>><a href="login.php">Sign In</a>
@@ -21,11 +22,11 @@
             <li <? if($ACTIVEPAGE=='events'){?>class="active"<? }?>><a href="events.php">Events</a></li>
             <li <? if($ACTIVEPAGE=='gallery'){?>class="active"<? }?>><a href="gallery.php">Gallery</a></li>
             <li <? if($ACTIVEPAGE=='news'){?>class="active"<? }?>><a href="news.php">News</a></li>
+            <li <? if($ACTIVEPAGE=='about'){?>class="active"<? }?>><a href="about.php">About</a></li>
             <li <? if(!$_SESSION['UsErId'] || $_SESSION['UsErId']<0){?>
                     <? if($ACTIVEPAGE=='register'){ ?>class="active"<? }?>><a href="register.php">Auditions</a>
-                <? } else{ if($ACTIVEPAGE=='myaccount'){?>class="active"<? }?>><a href="myaccount.php">Account</a><? }?>
+                <? } else{ if($ACTIVEPAGE=='my_account'){?>class="active"<? }?>><a href="my_account.php">My Account</a><? }?>
             </li>
-            <li <? if($ACTIVEPAGE=='about'){?>class="active"<? }?>><a href="about.php">About</a></li>
         </ul>
     </nav>
 </header>
