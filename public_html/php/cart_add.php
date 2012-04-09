9b95c8c5c32c059204afa1b420349e27cfe6f2a9
Saki Sato <sakiisato@gmail.com>
2012-04-09 19:45:51 -0400

    moved php pages for cart into this php template folder

---
+++ public_html/php/cart_add.php
@@ -0,0 +1,21 @@
+<? include("../../connect.php");
+    if(isset($_REQUEST['rmv']) && $_REQUEST['rmv'] == "yes")
+    {
+        GTG_remove_from_cart("#".$_REQUEST['sku']);
+    }
+    else
+    {
+        $q = trim($_REQUEST['quantity']);
+        //$cust_temp=$_REQUEST['db_cust'];
+        //$ourprice=GetName1("events_pricelevel","onlineprice","id",trim($_REQUEST['HidPriceid']));    
+        $ourprice=GetTicketPrice($_REQUEST['HidPriceid']);    
+        $size=trim($_REQUEST['HidPriceid']);
+        if(is_numeric($q))
+        {
+            GTG_add_to_cart($_REQUEST['HidPid'],$_REQUEST['quantity'],$ourprice,$size);
+        }        
+    }
+?>
+<script language="javascript">
+    location.href = "../cart.php";
+</script>
