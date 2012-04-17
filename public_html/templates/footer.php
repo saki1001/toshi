66292967d06297df47c46949a3381d9e47c7311f
Saki Sato <sakiisato@gmail.com>
2012-04-16 20:26:16 -0400

    added contact link and social media links"

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -1,13 +1,26 @@
 <footer>
     <section class="footer_wrapper">
         <ul class="social_media">
-            <li><a href="#" target="_blank" title="Facebook"><img src="http://www.placehold.it/20x20" width="20" height="20" alt="f" /></a></li>
-            <li><a href="#" target="_blank" title="Twitter"><img src="http://www.placehold.it/20x20" width="20" height="20" alt="t" /></a></li>
+            <li>
+                <a href="#" target="_blank" title="Facebook">
+                    <img src="images/icon-facebook.gif" width="18" height="18" alt="f" />
+                </a>
+            </li>
+            <li>
+                <a href="#" target="_blank" title="Twitter">
+                    <img src="images/icon-twitter.gif" width="18" height="18" alt="t" />
+                </a>
+            </li>
         </ul>
         <ul class="contact_info">
-            <li>Toshi's Living Room</li>
+            <li>
+                <h3>Toshi's Living Room</h3>
+            </li>
             <li>1141 Broadway</li>
             <li>New York, NY 10001</li>
+            <li>
+                <a href="mailto:<? echo $REPRESENTATIVE_MAIL; ?>">Contact Us</a>
+            </li>
         </ul>
     </section>
 </footer>
