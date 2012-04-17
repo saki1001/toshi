d53a4f5a9a67426f1432be600888d3b00fbe50aa
Saki Sato <sakiisato@gmail.com>
2012-04-17 18:03:46 -0400

    added social links

--- public_html/templates/footer.php
+++ public_html/templates/footer.php
@@ -2,12 +2,12 @@
     <section class="footer_wrapper">
         <ul class="social_media">
             <li>
-                <a href="#" target="_blank" title="Facebook">
+                <a href="https://www.facebook.com/pages/Toshis-Living-Room/380277968659709" title="Facebook">
                     <img src="images/icon-facebook.gif" width="18" height="18" alt="f" />
                 </a>
             </li>
             <li>
-                <a href="#" target="_blank" title="Twitter">
+                <a href="http://twitter.com/#!/toshislivingrm" title="Twitter">
                     <img src="images/icon-twitter.gif" width="18" height="18" alt="t" />
                 </a>
             </li>
