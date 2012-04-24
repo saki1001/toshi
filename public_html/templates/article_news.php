421e2092ac6ae07747b1cfdea53b69573832f504
Saki Sato <sakiisato@gmail.com>
2012-04-23 21:20:43 -0400

    now news thumb is defined in define_news

--- public_html/templates/article_news.php
+++ public_html/templates/article_news.php
@@ -3,12 +3,7 @@
 <article class="<? echo $articleType; ?>">
     <a href="<? echo $newsDetailLink ?>" class="event_detail_link">
         <div class="thumb">
-        <? if($newsRow['picture']!='' && $newsRow['picture_display']=='Y'){?>
             <img src="<? echo $newsThumbLink; ?>" height="80" alt="thumbnail" />
-        <? }else{ ?>
-            <!-- TODO: replace with image of toshi -->
-            <img src="images/default_article_thumb.jpg" width="120" height="80" alt="thumbnail" />
-        <? } ?>
         </div>
         <div class="info">
             <h2><? echo ucfirst(stripslashes($newsRow['name']));?></h2>
