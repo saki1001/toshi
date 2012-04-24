18f93d7a37f1295151a32cb16636f82be7ef9e7e
Saki Sato <sakiisato@gmail.com>
2012-04-23 21:17:04 -0400

    added calendar

--- public_html/events.php
+++ public_html/events.php
@@ -22,26 +22,35 @@
     <!-- CONTENT -->
         <div id="content">
             <h2 class="page_title"><? echo $PAGETITLE; ?></h2>
-            <h3 class="info_title">Upcoming Events</h3>
             <section id="calendar_wrapper">
                 <div id="calendar">
+                    <? include("templates/calendar.php");?>
                 </div>
             </section>
             <section class="article_list">
                 <?
+                include("templates/calendar_sort.php");
+                
                 // EVENT TYPE and CSS CLASSES
                 $eventType = 'CURRENT';
                 $articleType = "full_width";
-            
-                // GETTING EVENTS (NEXT 30)
-                $eventQuery = "SELECT * FROM events WHERE approve='Y' AND startdate >= CURDATE() ORDER BY startdate ASC LIMIT 30";
+                
+                // GETTING EVENTS
+                $eventQuery = "SELECT * FROM events WHERE approve='Y' $andqry ORDER BY startdate ASC";
                 $eventResult = mysql_query($eventQuery) or die(mysql_error());
-            
-                // POPULATE EVENTS
-                while($eventRow = mysql_fetch_array($eventResult)){
-                    $eventId = $eventRow['id'];
-                    include("templates/article_event.php");
+                $totalEvents = mysql_affected_rows();
+                // $result = $prs_pageing->number_pageing_front($cartsql,10,10,"Y","Y");
+                
+                if($totalEvents > 0) {
+                    // POPULATE EVENTS
+                    while($eventRow = mysql_fetch_array($eventResult)){
+                        $eventId = $eventRow['id'];
+                        include("templates/article_event.php");
+                    }
+                } else {
+                    echo "<p class='empty'>No events found.</p>";
                 }
+                
                 ?>
             </section>
         </div>
