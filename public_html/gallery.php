8a970c0a25b986541a206e8c1dbdb8a179ae2c5e
Saki Sato <sakiisato@gmail.com>
2012-04-25 19:32:47 -0400

    added past event sort

--- public_html/gallery.php
+++ public_html/gallery.php
@@ -11,7 +11,9 @@
     
 <!-- HEAD -->
     <? include("templates/head.php");?>
+    <link rel="stylesheet" href="css/gallery.css" type="text/css" media="all">
     
+    <script src="js/gallery.js" type="text/javascript"></script>
 </head>
 
 <body id="<? echo $ACTIVEPAGE; ?>">
@@ -24,13 +26,11 @@
             <h2 class="page_title"><? echo $PAGETITLE; ?></h2>
             <section id="calendar_wrapper">
                 <div id="calendar">
-                    <? include("templates/calendar.php");?>
+                    <? include("templates/gallery_event_sort.php");?>
                 </div>
             </section>
             <section class="article_list">
-                <?
-                include("templates/calendar_sort.php");
-                
+                <?                
                 // EVENT TYPE and CSS CLASSES
                 $eventType = 'PAST';
                 $articleType = "two_column";
@@ -49,16 +49,6 @@
                 } else {
                     echo "<p class='empty'>No events found.</p>";
                 }
-                
-                // // GETTING EVENTS (PAST 30)
-                // $eventQuery = "SELECT * FROM events WHERE approve='Y' AND startdate < CURDATE() ORDER BY startdate DESC LIMIT 30";
-                // $eventResult = mysql_query($eventQuery) or die(mysql_error());
-                // 
-                // // POPULATE EVENTS
-                // while($eventRow = mysql_fetch_array($eventResult)){
-                //     $eventId = $eventRow['id'];
-                //     include("templates/article_event.php");
-                // }
                 
                 ?>
             </section>
