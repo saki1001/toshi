be3c69f7c3661c99c384cf17b8759844f71d3f94
Saki Sato <sakiisato@gmail.com>
2012-04-30 21:47:04 -0400

    moved script to separate file, bound enter key to submit

--- public_html/login.php
+++ public_html/login.php
@@ -11,7 +11,8 @@
     
 <!-- HEAD -->
      <? include("templates/head.php");?>
-    
+     
+     <script src="js/login.js" type="text/javascript"></script>
 </head>
 
 <body id="<? echo $ACTIVEPAGE; ?>">
@@ -48,88 +49,5 @@
     </div>
 <!-- FOOTER -->
     <? include("templates/footer.php");?>
-<!-- SCRIPT -->
-    <script language="javascript" type="text/javascript">
-        $(document).ready( function() {            
-            
-            var submitForm = function() {
-                var email = $('form #email');
-                var password = $('form #password');
-                var emailVal = email.val();
-                var passwordVal = password.val();
-                
-                // unbind to prevent multiple submits
-                $('#submit').unbind('click', submitForm);
-                
-                if(emailVal === '') {
-                    $('#msg').addClass('active')
-                        .html('Please enter your email address.');
-                    email.focus();
-                    // rebind
-                    $('#submit').bind('click', submitForm);
-                    return false;
-                }
-                if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.val()))) {
-                    $('#msg').addClass('active')
-                        .html('Please enter a proper email address.');
-                    email.focus();
-                    // rebind
-                    $('#submit').bind('click', submitForm);
-                    return false;
-                }
-                if(passwordVal === '') {
-                    $('#msg').addClass('active')
-                        .html("Please enter your password.");
-                    password.focus();
-                    // rebind
-                    $('#submit').bind('click', submitForm);
-                    return false;
-                }
-                
-                $.ajax({
-                    url: 'php/login_submit.php',
-                    type: "POST",
-                    data: {
-                      email: emailVal,
-                      password: passwordVal
-                      },
-                    dataType: 'json',
-                    success: function(data) {
-                        if(data['status'] === 'ERROR_INVALID') {
-                            $('#msg').addClass('active')
-                                .html('Invalid email or password.');
-                            // rebind submit
-                            $('#submit').bind('click', submitForm);
-                            
-                        } else if(data['status'] === 'ERROR_CONFIRM') {
-                            $('#msg').addClass('active')
-                                .html('Please check your email and confirm your account.');
-                            // rebind submit
-                            $('#submit').bind('click', submitForm);
-                                
-                        } else if(data['status'] === 'SUCCESS') {
-                            // send to account page
-                            document.location = 'my_account.php';
-                            
-                        } else {
-                            $('#msg').addClass('active')
-                                .html('Unable to sign in. Please contact us.');
-                            $('#submit').bind('click', submitForm);
-                        }
-                      },
-                      error: function (xhr, ajaxOptions, thrownError) {
-                          alert(xhr.statusText);
-                          alert(xhr.responseText);
-                          alert(xhr.status);
-                          alert(thrownError);
-                      }
-                });
-                
-                return false;
-            };
-            
-            $('#submit').bind('click', submitForm);
-        });
-    </script>
 </body>
 </html>
