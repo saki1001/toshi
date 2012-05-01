24ff06f6e79800c67a88c269c4671843eae27a10
Saki Sato <sakiisato@gmail.com>
2012-04-30 21:44:51 -0400

    moved script to separate file

--- public_html/forgot_password.php
+++ public_html/forgot_password.php
@@ -11,7 +11,8 @@
     
 <!-- HEAD -->
      <? include("templates/head.php");?>
-    
+     
+     <script src="js/forgot_password.js" type="text/javascript"></script>
 </head>
 
 <body id="<? echo $ACTIVEPAGE; ?>">
@@ -42,74 +43,5 @@
     </div>
 <!-- FOOTER -->
     <? include("templates/footer.php");?>
-<!-- SCRIPT -->
-    <script language="javascript" type="text/javascript">
-        $(document).ready( function() {
-        
-            var submitForm = function() {
-                var email = $('form #email');
-                var emailVal = email.val();
-            
-                // unbind to prevent multiple submits
-                $('#submit').unbind('click', submitForm);
-            
-                if(emailVal === '') {
-                    $('#msg').addClass('active')
-                        .html('Please enter your email address.');
-                    email.focus();
-                    // rebind
-                    $('#submit').bind('click', submitForm);
-                    return false;
-                }
-                if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.val()))) {
-                    $('#msg').addClass('active')
-                        .html('Please enter a proper email address.');
-                    email.focus();
-                    // rebind
-                    $('#submit').bind('click', submitForm);
-                    return false;
-                }
-            
-                $.ajax({
-                    url: 'php/ajax_forgot.php',
-                    type: "POST",
-                    data: {
-                      email: emailVal
-                    },
-                    dataType: 'json',
-                    success: function(data) {
-                        if(data['status'] === 'NOT_FOUND') {
-                            $('#msg').addClass('active')
-                                .html('Sorry, we can\'t find that email in our records.');
-                            // rebind submit
-                            $('#submit').bind('click', submitForm);
-                        
-                        } else if(data['status'] === 'SUCCESS') {
-                            // send to account page
-                            $('#msg').addClass('active')
-                                .html('Your password has been emailed to ' + emailVal + ".");
-                        
-                        } else {
-                            $('#msg').addClass('active')
-                                .html('Unknown error. Please contact us.');
-                            // rebind submit
-                            $('#submit').bind('click', submitForm);
-                        
-                        }
-                      },
-                      error: function (xhr, ajaxOptions, thrownError) {
-                          alert(xhr.statusText);
-                          alert(xhr.responseText);
-                          alert(xhr.status);
-                          alert(thrownError);
-                      }
-                });
-                
-                return false;
-            };
-            
-            $('#submit').bind('click', submitForm);
-        });
-    </script>
 </body>
 </html>
