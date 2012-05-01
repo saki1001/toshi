d04786d1c4d6be5aae8d7e1ad4174b0a8577cd4c
Saki Sato <sakiisato@gmail.com>
2012-04-30 21:52:08 -0400

    tried html5 player for video

--- public_html/my_videos_detail.php
+++ public_html/my_videos_detail.php
@@ -1,20 +1,19 @@
-<? include("../connect.php"); 
-    $mlevel=3;
-    $userid=$_GET["userid"];
-    $id=$_GET["id"];
-?>
+<? include("../connect.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <!-- VARIBLES -->
-    <?
-        $ACTIVEPAGE='my_account';
-        $PAGETITLE='';
-        
-        $getVideos="SELECT * FROM users_videos WHERE userid='".trim($userid)."' and id='".trim($id)."' order by id desc";
+    <?  
+        $userid=$_GET["userid"];
+        $id=$_GET["id"];
+        $getVideos="SELECT * FROM users_videos WHERE userid='".trim($userid)."' AND id='".trim($id)."' ";
         $getVideosResult=mysql_query($getVideos);
-        $totalVideos=mysql_affected_rows();
         $getVideosRow = mysql_fetch_array($getVideosResult);
+        $totalVideos=mysql_affected_rows();
+        
+        $ACTIVEPAGE = 'my_account';
+        $PAGETITLE = ucfirst(stripslashes($getVideosRow['caption']));
     ?>
     
 <!-- HEAD -->
@@ -24,18 +23,29 @@
 
 <body id="<? echo $ACTIVEPAGE; ?>">
 <!-- CONTENT -->
-    <div id="content"><script type="text/javascript" src="js/flowplayer-3.2.6.min.js"></script>
-        <h2 class="page_title"><? echo ucfirst(stripslashes($getVideosRow['caption'])); ?><h2>
+    <div id="content">
         <? if($totalVideos>0) { ?>
-            <? if($getVideosRow['video']!=""){?>
+            <h2 class="page_title">
+                <? echo ucfirst(stripslashes($getVideosRow['caption'])); ?>
+            </h2>
+            <? if($getVideosRow['video']!=""){ ?>
                 <? echo stripslashes($getVideosRow['video']); ?>
             <? } else if($getVideosRow['videofile']!="") { ?>
+                <!-- <script type="text/javascript" src="js/flowplayer-3.2.6.min.js"></script>
                 <a  href="<?=$SITE_URL;?>/Videos/<?=stripslashes($getVideosRow['videofile']);?>"  id="player"></a> 
-                <script language="javascript1.1">flowplayer("player", "flowplayer-3.2.7.swf");</script>
-            <? } else { }?>
-        <? } else ?>
-            No Videos Uploaded.
-        <? } >?>
+                <script language="javascript1.1">flowplayer("player", "flowplayer-3.2.7.swf");</script> -->
+                <video width="320" height="240" controls="controls">
+                  <source src="<?=$SITE_URL;?>/Videos/<?=stripslashes($getVideosRow['videofile']);?>" />
+                  Your browser does not support the video tag.
+                </video>
+            <? } else {
+                    echo 'Unable to locate video.';
+                }
+            ?>
+        <? } else {
+            echo 'No Videos Uploaded.';
+            }
+        ?>
     </div>
 </body>
 </html>
